0123f336ab5ffa0a388142a580589425ffcc8ad7
Rossitek1 <rossitek1@rossitek1s-mac-mini.local>
2018-03-22 11:34:46 +0530

    music-player-app

---
+++ music app/View/MusicPlayerCellCollectionViewCell.swift
@@ -0,0 +1,17 @@
+
+//
+//  MusicPlayerCellCVC.swift
+//  music app
+//
+//  Created by Rossitek1 on 21/03/18.
+//  Copyright © 2018 Rossitek. All rights reserved.
+//
+import UIKit
+
+class MusicPlayerCellCollectionViewCell: UITableViewCell {
+    
+    @IBOutlet var imageMusic: UIImageView!
+    @IBOutlet var labelMusicTitle: UILabel!
+    @IBOutlet var labelMusicDescription: UILabel!
+    
+}
